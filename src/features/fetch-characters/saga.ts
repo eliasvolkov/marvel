c569768695c5a37dbcdd646dcec4b3f6a3ef681e
User <you@example.com>
2019-09-23 18:23:34 +0300

    add sagas for fetching by page

--- src/features/fetch-characters/saga.ts
+++ src/features/fetch-characters/saga.ts
@@ -1,8 +1,14 @@
 import { IResults } from "./../../shared/models/response";
-import { fetchCharactersApi } from "./../../shared/api/index";
-import { fetchCharacters, fetchCharactersRequest } from "./actions";
+import {
+  fetchCharactersApi,
+  fetchCharactersApiByPage
+} from "./../../shared/api/index";
+import { fetchCharacters, fetchCharactersByPage } from "./actions";
 import { call, takeEvery, put } from "redux-saga/effects";
-import { FETCH_CHARACTERS_REQUEST } from "./constants";
+import {
+  FETCH_CHARACTERS_REQUEST,
+  FETCH_CHARACTERS_BY_PAGE_REQUEST
+} from "./constants";
 
 export function* fetchCharactersAsync(): Generator {
   const characters: any = yield call(fetchCharactersApi);
@@ -11,4 +17,16 @@ export function* fetchCharactersAsync(): Generator {
 
 export function* fetchCharactersStart() {
   yield takeEvery(FETCH_CHARACTERS_REQUEST, fetchCharactersAsync);
+}
+
+//Fetching By Page
+
+export function* fetchCharactersByPageAsync(action: any) {
+  console.log(action.payload);
+  const characters: any = yield call(fetchCharactersApiByPage, action.payload);
+  yield put(fetchCharactersByPage(characters));
+}
+
+export function* fetchCharactersByPageStart() {
+  yield takeEvery(FETCH_CHARACTERS_BY_PAGE_REQUEST, fetchCharactersByPageAsync);
 }
