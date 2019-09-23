09e360cd2a0f46de747f9a2c465ee65c1912d352
User <you@example.com>
2019-09-23 18:25:16 +0300

    add new api for fetching by page

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -3,7 +3,20 @@ export const fetchCharactersApi = async () => {
   const hash: string = "e9af80b0c13e7a268a152c6352dac9b8";
 
   const response = await fetch(
-    `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${apiKey}&hash=${hash}`
+    `https://gateway.marvel.com:443/v1/public/characters?&limit=80&ts=1&apikey=${apiKey}&hash=${hash}`
+  );
+  const { data } = await response.json();
+
+  return data.results;
+};
+
+export const fetchCharactersApiByPage = async (page: number) => {
+  const apiKey: string = "4c9674f31e041ad25ef153635461556e";
+  const hash: string = "e9af80b0c13e7a268a152c6352dac9b8";
+
+  const response = await fetch(
+    `https://gateway.marvel.com:443/v1/public/characters?&limit=10&offset=${page *
+      10}&ts=1&apikey=${apiKey}&hash=${hash}`
   );
   const { data } = await response.json();
 
