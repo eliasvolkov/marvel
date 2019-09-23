0ae5f37aa8a4cc0fbb663b5a2516c2726379ffcc
User <you@example.com>
2019-09-23 15:55:50 +0300

    Test App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Layout } from "./shared/components/Layout";
+import CharacterContainer from "./features/fetch-characters/components/CharacterContainer";
 
 const App: React.FC = () => {
   return (
     <Layout>
       <h1>Loading...</h1>
+      <CharacterContainer />
     </Layout>
   );
 };
