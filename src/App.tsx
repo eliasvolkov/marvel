d5100ca21632de0fd7ee7a1932763c0988c2776b
User <you@example.com>
2019-09-23 11:29:18 +0300

    add Layout to folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Layout } from "./shared/Layout";
+import { Layout } from "./shared/components/Layout";
 
 const App: React.FC = () => {
   return (
