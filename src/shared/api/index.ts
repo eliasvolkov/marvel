ae2ae4ff470e58b95304e9025b2d60a51b3ad08f
User <you@example.com>
2019-09-23 11:29:43 +0300

    create API function

---
+++ src/shared/api/index.ts
@@ -0,0 +1,11 @@
+export const fetchCharacters = async () => {
+  const apiKey: string = "4c9674f31e041ad25ef153635461556e";
+  const hash: string = "e9af80b0c13e7a268a152c6352dac9b8";
+
+  const response = await fetch(
+    `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${apiKey}&hash=${hash}`
+  );
+  const { data } = await response.json();
+
+  return data.results;
+};
