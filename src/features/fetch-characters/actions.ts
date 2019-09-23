4c19b71259e914f0543c1054f5a46782ee3d90b4
User <you@example.com>
2019-09-23 18:22:36 +0300

    add actions for fetching by page

--- src/features/fetch-characters/actions.ts
+++ src/features/fetch-characters/actions.ts
@@ -31,3 +31,41 @@ export const fetchCharacters = (
     payload
   };
 };
+
+/* Fetching results by page */
+
+export interface FetchCharactersByPageRequest {
+  type: constants.FETCH_CHARACTERS_BY_PAGE_REQUEST;
+  payload: number;
+}
+export interface FetchCharactersByPageSuccess {
+  type: constants.FETCH_CHARACTERS_BY_PAGE_SUCCESS;
+  payload: Array<IResults>;
+}
+export interface FetchCharactersByPageFailure {
+  type: constants.FETCH_CHARACTERS_BY_PAGE_FAILURE;
+  payload: string;
+}
+
+export type FetchCharactersByPageAction =
+  | FetchCharactersByPageRequest
+  | FetchCharactersByPageSuccess
+  | FetchCharactersByPageFailure;
+
+export const fetchCharactersByPageRequest = (
+  page: number
+): FetchCharactersByPageRequest => {
+  return {
+    type: constants.FETCH_CHARACTERS_BY_PAGE_REQUEST,
+    payload: page
+  };
+};
+
+export const fetchCharactersByPage = (
+  payload: Array<IResults>
+): FetchCharactersByPageSuccess => {
+  return {
+    type: constants.FETCH_CHARACTERS_BY_PAGE_SUCCESS,
+    payload
+  };
+};
