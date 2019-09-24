c5148cf3aed0f5adc722c78b6fcd0118d0a9b08e
Ilya Volkov <ilya.volkov@noveogroup.com>
2019-09-24 12:18:10 +0300

    add api for startsWithName

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -22,3 +22,15 @@ export const fetchCharactersApiByPage = async (page: number) => {
 
   return data.results;
 };
+
+export const nameStartsWithApi = async (name: string) => {
+  const apiKey: string = "4c9674f31e041ad25ef153635461556e";
+  const hash: string = "e9af80b0c13e7a268a152c6352dac9b8";
+
+  const response = await fetch(
+    `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${name}&ts=1&apikey=${apiKey}&hash=${hash}`
+  );
+  const { data } = await response.json();
+
+  return data.results;
+};
